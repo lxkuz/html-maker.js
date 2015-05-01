40fcb7ce5dde7962175abec25efe1d6796a3a2d7
Alexey <kuznetsov@mystand.ru>
2015-05-01 23:20:12 +0600

    build script fix, local npm packages, removed jake

--- Jakefile.js
+++
@@ -1,3 +0,0 @@
-task("default", function() {
-    console.log("TODO make build task");
-});
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 coffee --map --compile --output src src/*.coffee
-
 cjsify -o build/helper.js  src/helper.js
-cjsify -o build/html-maker.js  src/html-maker.js
-uglifyjs -o build/htmlmake.min.js -d build/*.js
+cjsify -o build/html-maker.js src/html-maker.js
+uglifyjs -o htmlmake.min.js -d build/*.js
 
